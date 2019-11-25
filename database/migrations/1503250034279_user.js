11b0415b747baee4a61cf6088b3bf29aff23931c
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2019-11-25 14:37:51 -0300

    relacionametos user_image

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -5,13 +5,19 @@ const Schema = use('Schema')
 
 class UserSchema extends Schema {
   up () {
-    this.create('users', (table) => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
-      table.timestamps()
-    })
+      this.create('users', (table) => {
+          table.increments()
+          table.string('name',80)
+          table.string('surname',200)
+          table
+              .string('email', 254)
+              .notNullable()
+              .unique()
+
+          table.string('password', 60).notNullable()
+          table.integer('image_id').unsigner()
+          table.timestamps()
+      })
   }
 
   down () {
