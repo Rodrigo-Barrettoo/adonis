cdfd545b4048d0c088b5c75293fefe8132fc8e99
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2019-11-25 15:58:04 -0300

    order items

--- database/migrations/1574706551224_cupon_order_schema.js
+++ database/migrations/1574706551224_cupon_order_schema.js
@@ -4,12 +4,12 @@
 const Schema = use('Schema')
 
 class CuponOrderSchema extends Schema {
-  up () {
+  up() {
     this.create('cupon_order', (table) => {
       table.increments()
       table.integer('coupon_id').usingned()
       table.integer('order_id').usingned()
-      table.decimal('discount',12, 2).defaultTo(0.0)
+      table.decimal('discount', 12, 2).defaultTo(0.0)
 
       table.foreign('coupon_id').references('id').inTable('coupons').onDelete('cascade').onUpdate('cascade')
       table.foreign('order_id').references('id').inTable('orders').onDelete('cascade').onUpdate('cascade')
@@ -18,7 +18,7 @@ class CuponOrderSchema extends Schema {
     })
   }
 
-  down () {
+  down() {
     this.drop('cupon_order')
   }
 }
