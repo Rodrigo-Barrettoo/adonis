76bceacf511b510fb1afd464469faab4048c9b21
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2019-11-27 10:29:21 -0300

    role and client seeders

---
+++ database/seeds/0002_ClientSeeder.js
@@ -0,0 +1,39 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| 0002_ClientSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+const Role = use('Role')
+const User = use('App/Models/User')
+
+class ClientSeeder {
+  async run() {
+    const role = await Role.findBy('slug', 'client')
+    const Clientes = await Factory.model('App/Models/User').createMany(30)
+    await Promise.all(clients.map(async client => {
+        await client.roles().attach([role.id])
+      })
+    )
+
+    const user = await User.create({
+      name: 'Rodrigo',
+      surname: 'Barreto',
+      email: 'rodrigo@adonis.com.br',
+      password: 'secret',
+    })
+
+    const adminRole = await  Role.findBy('slug', 'admin')
+    await user.roles().attach([role.id])
+  }
+}
+
+module.exports = ClientSeeder
