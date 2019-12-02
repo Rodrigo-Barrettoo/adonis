18c8c7b38dd11ab559c0607d537bb7f81801108d
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2019-12-02 10:42:18 -0300

    Controller Order modificado para melhorias

--- app/Controllers/Http/Admin/OrderController.js
+++ app/Controllers/Http/Admin/OrderController.js
@@ -3,6 +3,10 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
+const Order = use('App/Models/Order');
+const Database = use('Database');
+const Service = use('App/Services/Order/OrderService');
+
 
 /**
  * Resourceful controller for interacting with orders
@@ -17,19 +21,24 @@ class OrderController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
+  async index({request, response, pagination}) {
+    const {status, id} = request.only(['status', 'id']);
+    const query = Order.query();
 
-  /**
-   * Render a form to be used for creating a new order.
-   * GET orders/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+    if (status && id) {
+      query.where('status', status);
+      query.orWhere('id', 'LIKE'`%${id}%`)
+
+    } else if (status) {
+      query.where('status', status);
+
+    } else if (id) {
+      query.orWhere('id', 'LIKE'`%${id}%`);
+
+    }
+
+    const orders = query.paginate(pagination.page, pagination.limit);
+    return response.send(orders);
   }
 
   /**
@@ -40,7 +49,27 @@ class OrderController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({request, response}) {
+    const trx = await Database.beginTransaction();
+
+    try {
+      const {user_id, items, status} = request.all();
+      let order = await Order.create({user_id, status}, trx);
+      const service = new Service(order, trx);
+
+      if (items && items.length > 0) {
+        await service.syncItems(items);
+      }
+
+      await trx.commit();
+      return response.status(201).send(order)
+
+    } catch (e) {
+      await trx.rollback();
+      return response.status(400).send({
+        message: 'Não foi possivel criar o pedido no momento!'
+      })
+    }
   }
 
   /**
@@ -52,19 +81,9 @@ class OrderController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing order.
-   * GET orders/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async show({params: {id}, request, response}) {
+    const order = await Order.findOrFail(id);
+    return response.send(order);
   }
 
   /**
@@ -75,7 +94,25 @@ class OrderController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({params: {id}, request, response}) {
+    const order = await Order.findOrFail(id);
+    const trx = await Database.beginTransaction();
+
+    try {
+      const {user_id, items, status} = request.all();
+      order.merge({user_id, status});
+      const service = await new Service(order, trx);
+      await service.updateItems(items);
+      await order.save(trx);
+      await trx.commit();
+
+      return response.send(order);
+    } catch (e) {
+      await trx.rollback();
+      return response.status(400).send({
+        message: 'Não foi possivel atualizar o pedido no momento!'
+      });
+    }
   }
 
   /**
@@ -86,7 +123,23 @@ class OrderController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({params: {id}, request, response}) {
+    const order = await Order.findOrFail(id);
+    const trx = await Database.beginTransaction();
+
+    try {
+      await order.items().delete(trx);
+      await order.coupons().delete(trx);
+
+      await order.delete(trx);
+      await trx.commit();
+      return response.status(204).send()
+    } catch (e) {
+      await trx.rollback();
+      return response.status(400).send({
+        message: 'Erro ao deletar este pedido!'
+      })
+    }
   }
 }
 
