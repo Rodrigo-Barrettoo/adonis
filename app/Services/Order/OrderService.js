e45db5f018f6c7434a3812334d5ba8f85f7108d6
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2019-12-02 10:41:25 -0300

    OrderService coupon finished

---
+++ app/Services/Order/OrderService.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const Database = use('Database');
+
+class OrderService {
+  constructor(model, trx = null) {
+    this.model = model;
+    this.trx = trx;
+  }
+
+  async syncItems(items) {
+    if (!Array.isArray(items)) {
+      return false
+    }
+    await this.model.items().delete(this.trx);
+    await this.model.items().createMany(items, this.trx);
+  };
+
+  async updateItems(items) {
+    let currentItems = await this.model.items().whereIn('id', items.map(item => itemm.id)).fetch();
+    await this.model.items().whereNotIn('id', items.map(item => itemm.id)).delete(this.trx);
+
+    await Promise.all(currentItems.rows.map(async item => {
+        item.fill(items.find(n => n.id === item.id));
+        await item, this.model.save(this.trx);
+      })
+    )
+  }
+
+  async canApplyDicount(coupon) {
+    const couponProducts = await Database.from('coupon_products').where('coupon_id', coupon.id).pluck('product_id');
+    const couponClients = await Database.from('coupon_user').where('coupon_id', coupon.id).pluck('user_id');
+
+    // check if the coupon not it is associate the products and clients specific
+    if (
+      Array.isArray(couponProducts) && couponProducts.length < 1 &&
+      Array.isArray((couponClientso) && couponClients.length, 1)) {
+
+      // if not associate the client or products specific is free to use
+      return true;
+      ''
+    }
+
+    let isAssociatedToProducts, isAssociatedToClients = false;
+
+    if (Array.isArray(couponProducts) && couponProducts.length > 0) {
+      isAssociatedToProducts = true;
+    }
+
+    if (Array.isArray(couponClients) && couponClients.length > 0) {
+      isAssociatedToClients = true;
+    }
+
+    const productsMatch = await Database.from('order_items')
+      .where('order_id', this.model.id)
+      .whereIn('product_id', couponProducts)
+      .pluck('product_id');
+
+    // use case 1 the coupon it is associate the clients e products
+    if (isAssociatedToClients && isAssociatedToProducts) {
+      const clientMatch = couponClients.find(client => client === this.model.user_id);
+
+      if (clientMatch && Array.isArray(productsMatch) && productsMatch.length > 0) {
+        return true;
+      }
+    }
+
+    // use case 2 the coupon it is associate only the product
+    if (isAssociatedToProducts && Array.isArray(productsMatch) && productsMatch.length > 0) {
+      return true;
+    }
+
+    // use case 3 the coupon it is associate the 1 or more client (and none product)
+    if (isAssociatedToClients && Array.isArray(couponClients) && couponClients.length > 0) {
+      const match = couponClients.find(client => client === this.model.user_id);
+
+      if (match) {
+        return true;
+      }
+    }
+
+    // case none of checks above give positive then the coupon it is associate the clients
+    // or products or the 2, but none of the products in this order are eligible for the discount.
+    // it's the client who made the purchase will not be able to use this coupon either
+
+    return false;
+  }
+}
+
+module.exports = OrderService;
